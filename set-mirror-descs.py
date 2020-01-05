ceb83ccac9fc590b1e973cb1d9d1fd465a4a23a7
Michał Górny <mgorny@gentoo.org>
2020-01-05 07:48:48 +0100

    set-mirror-descs.py: Prefix non-mirrors as well
    Signed-off-by: Michał Górny <mgorny@gentoo.org>

--- set-mirror-descs.py
+++ set-mirror-descs.py
@@ -77,6 +77,32 @@ def update_mirrors(mirrors, gh):
             print('-> updated')
 
 
+def update_nonmirrors(mirrors, gh):
+    o = gh.get_organization('gentoo')
+
+    all_mirrors = frozenset('gentoo/{}'.format(r.ghname) for r in mirrors)
+    for ghr in o.get_repos():
+        if ghr.full_name in all_mirrors:
+            continue
+        print('non-mirror: {}'.format(ghr.full_name))
+        # gh redirects transferred repos
+        if ghr.organization is None:
+            print('-> not in gentoo')
+            continue
+        if ghr.description is None:
+            new_desc = '[ORIGIN] (no description)'
+        elif ghr.description.startswith('[ORIGIN]'):
+            continue
+        else:
+            assert not ghr.description.startswith('[MIRROR]')
+            new_desc = '[ORIGIN] {}'.format(ghr.description)
+        if ghr.archived:
+            print('-> archived')
+        else:
+            ghr.edit(description=new_desc)
+            print('-> updated')
+
+
 def main(argv):
     argp = argparse.ArgumentParser(prog=argv[0])
     argp.add_argument('--api-key-file', type=argparse.FileType('r'),
@@ -92,6 +118,7 @@ def main(argv):
 
     gh = github.Github(args.api_key_file.read().strip())
     update_mirrors(mirrors, gh)
+    update_nonmirrors(mirrors, gh)
 
 
 if __name__ == '__main__':
