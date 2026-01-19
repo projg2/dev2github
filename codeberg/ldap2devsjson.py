6a69f8ba4075cc0d10e993155c400d6ff0b22e7f
Thomas Bracht Laumann Jespersen <t@laumann.xyz>
2026-01-19 12:01:38 +0100

    codeberg: version of ldap2devsjson.py
    Signed-off-by: Thomas Bracht Laumann Jespersen <t@laumann.xyz>
    Part-of: https://github.com/projg2/dev2github/pull/4
    Signed-off-by: Michał Górny <mgorny@gentoo.org>

---
+++ codeberg/ldap2devsjson.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# Create devs.json from LDIF, Codeberg version
+
+import base64
+import json
+import sys
+
+
+def main(list_f="devs.ldif", devs_json="devs.json"):
+    devs = {}
+    with open(list_f) as f:
+        ldif_data = f.read()
+
+    for block in ldif_data.split("\n\n"):
+        if not block.strip():
+            continue
+        mails = set()
+        cbuser = ""
+        for l in block.splitlines():
+            k, v = l.split(":", 1)
+            if v.startswith(":"):
+                # base64
+                v = base64.b64decode(v[1:].strip()).decode()
+            else:
+                v = v.strip()
+
+            if k == "uid":
+                mails.add(v + "@gentoo.org")
+            elif k == "mail":
+                assert "@" in v
+                mails.add(v)
+            elif k == "gentooCodebergUser":
+                cbuser = v
+        assert mails
+        for m in mails:
+            devs[m] = cbuser
+
+    with open(devs_json, "w") as devs_f:
+        json.dump(devs, devs_f, indent=0, sort_keys=True)
+
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main(*sys.argv[1:]))
